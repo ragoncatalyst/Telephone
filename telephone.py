979bae024960fb9f1f2a52d4cf52b6f12c30e055
Ragon Lyu <ragon.lyu@gmail.com>
2023-09-29 19:58:58 +0800

    a

--- telephone.py
+++ telephone.py
@@ -58,6 +58,18 @@ if modech == "特殊行动":
         s4d = "     消耗所有怒气值，每1点怒气值，恢复3点生命值"
 
 
+
+
+
+
+
+
+
+
+
+
+
+
     print()
     print("[",chosench,"]生命值: ",php)
     print()
@@ -84,6 +96,7 @@ if modech == "特殊行动":
     d2hp = 0
     d3hp = 0
     roundnum = 0
+    roundcon = roundnum % 2
     talismanrage = 0
 
     print()
@@ -91,6 +104,7 @@ if modech == "特殊行动":
     print(e1)
     print("生命值: ",e1hp)
     print()
+    print("------------------------------------------------------------------")
 
     if chosench == "夜曲":
         print()
@@ -119,86 +133,88 @@ if modech == "特殊行动":
         if chosench == "夜曲":
             # player choose one skill to use
             # sun = skill used number
-            roundnum = roundnum + 1
-            print("------------------------------------------------------------------")
-            print()
-            print("[ 回合开始：第",roundnum,"回合 ]")
-            sun = input("输入技能对应数字并按下Enter：")
-            print()
-            if sun == "2" :
-                e1hp = e1hp - 5 - 3*dronenum
-                print("[ 我方行动 ]")
-                print("[夜曲]使用技能2[空中打击]")
-                print("[徘徊]受到",5+3*dronenum,"点物理伤害")
-                if e1hp < 0:
-                    break
-                else:
-                    print("[徘徊]剩余生命值: ",e1hp)
-            if sun == "1" :
-                if dronenum < 3:
-                    dronenum += 1
-                    if dronenum == 1:
-                        d1hp += 5
-                    elif dronenum == 2:
-                        d2hp += 5
-                    elif dronenum == 3:
-                        d3hp += 5
+            if roundcon == 1:
+                roundnum = roundnum + 1
+                print("------------------------------------------------------------------")
+                print()
+                print("[ 回合开始：第",roundnum,"回合 ]")
+                sun = input("输入技能对应数字并按下Enter：")
+                print()
+                if sun == "2" :
+                    e1hp = e1hp - 5 - 3*dronenum
                     print("[ 我方行动 ]")
-                    print("[夜曲]使用技能1[无人机升空]")
-                    print("当前[无人机]数量: ",dronenum)
-                    print()
-                    print("[徘徊]未受到攻击，生命值: ",e1hp)
-                    if dronenum > 3:
-                        dronenum = 3
-            if sun == "3":
-                playerarmor = playerarmor + dronenum * 1
-                if playerarmor > 10:
-                    playerarmor = 10
-                print("[ 我方行动 ]")
-                print("[夜曲]使用技能3[防御阵型]")
-                print("当前护甲层数: ", playerarmor)
-            if sun == "4":
-                healtarget = input("输入待修理无人机编号(01或02或03): ")
-                if healtarget == "01":
-                    d1hp += 3
-                    if d1hp > 5:
-                        d1hp = 5
-                elif healtarget == "02":
-                    d2hp += 3
-                    if d2hp > 5:
-                        d2hp = 5
-                elif healtarget == "03":
-                    d3hp += 3
-                    if d3hp > 5:
-                        d3hp = 5
-                
-            # drone condition
-            # drones = {'drone01': 100, 'drone02': 80, 'drone03': 60}  
-            # list(map(lambda drone: print(f'[{drone}]剩余生命值: {drones[drone]}'), drones))
-            if dronenum == 1:
-                print("[无人机01]剩余生命值: ",d1hp)
-            elif dronenum == 2:
-                print("[无人机01]剩余生命值: ",d1hp)
-                print("[无人机02]剩余生命值: ",d2hp)
-            elif dronenum == 3:
-                print("[无人机01]剩余生命值: ",d1hp)
-                print("[无人机02]剩余生命值: ",d2hp)
-                print("[无人机03]剩余生命值: ",d3hp)
+                    print("[夜曲]使用技能2[空中打击]")
+                    print("[徘徊]受到",5+3*dronenum,"点物理伤害")
+                    if e1hp < 0:
+                        break
+                    else:
+                        print("[徘徊]剩余生命值: ",e1hp)
+                if sun == "1" :
+                    if dronenum < 3:
+                        dronenum += 1
+                        if dronenum == 1:
+                            d1hp += 5
+                        elif dronenum == 2:
+                            d2hp += 5
+                        elif dronenum == 3:
+                            d3hp += 5
+                        print("[ 我方行动 ]")
+                        print("[夜曲]使用技能1[无人机升空]")
+                        print("当前[无人机]数量: ",dronenum)
+                        print()
+                        print("[徘徊]未受到攻击，生命值: ",e1hp)
+                        if dronenum > 3:
+                            dronenum = 3
+                if sun == "3":
+                    playerarmor = playerarmor + dronenum * 1
+                    if playerarmor > 10:
+                        playerarmor = 10
+                    print("[ 我方行动 ]")
+                    print("[夜曲]使用技能3[防御阵型]")
+                    print("当前护甲层数: ", playerarmor)
+                if sun == "4":
+                    healtarget = input("输入待修理无人机编号(01或02或03): ")
+                    if healtarget == "01":
+                        d1hp += 3
+                        if d1hp > 5:
+                            d1hp = 5
+                    elif healtarget == "02":
+                        d2hp += 3
+                        if d2hp > 5:
+                            d2hp = 5
+                    elif healtarget == "03":
+                        d3hp += 3
+                        if d3hp > 5:
+                            d3hp = 5
+                    
+                # drone condition
+                # drones = {'drone01': 100, 'drone02': 80, 'drone03': 60}  
+                # list(map(lambda drone: print(f'[{drone}]剩余生命值: {drones[drone]}'), drones))
+                if dronenum == 1:
+                    print("[无人机01]剩余生命值: ",d1hp)
+                elif dronenum == 2:
+                    print("[无人机01]剩余生命值: ",d1hp)
+                    print("[无人机02]剩余生命值: ",d2hp)
+                elif dronenum == 3:
+                    print("[无人机01]剩余生命值: ",d1hp)
+                    print("[无人机02]剩余生命值: ",d2hp)
+                    print("[无人机03]剩余生命值: ",d3hp)
 
-            # enemy attack, random pick target
-            print("[徘徊]发起攻击")
-            if dronenum == 1:
-                etarget = random.randint(0,1)
-            elif dronenum == 2:
-                etarget = random.randint(0,2)
-            elif dronenum == 3:
-                etarget = random.randint(0,3)
-            elif dronenum == 0:
-                etarget = 0
+            if roundcon == 0:
+                # enemy attack, random pick target
+                print("[徘徊]发起攻击")
+                if dronenum == 1:
+                    etarget = random.randint(0,1)
+                elif dronenum == 2:
+                    etarget = random.randint(0,2)
+                elif dronenum == 3:
+                    etarget = random.randint(0,3)
+                elif dronenum == 0:
+                    etarget = 0
 
-            # calculate damage caused by enemy
-            if etarget == 0:
-                print("攻击：[徘徊]-->[夜曲]")
+                # calculate damage caused by enemy
+                if etarget == 0:
+                    print("攻击：[徘徊]-->[夜曲]")
                 if playerarmor == 0:
                     php -= 2
                 print("[夜曲]受到2点物理伤害")
@@ -211,21 +227,22 @@ if modech == "特殊行动":
                     playerarmor = playerarmor - eatk
                     print("护甲挡下了",eatk,"点物理伤害，剩余",playerarmor,"层护甲")
                     print("[夜曲]未受到伤害")
-            elif etarget == 1:
-                print("攻击：[徘徊]-->[无人机01]")
-                d1hp = d1hp - eatk
-                if d1hp <= 0:
-                    d1hp = 0
-            elif etarget == 2:
-                print("攻击：[徘徊]-->[无人机02]")
-                d2hp = d2hp - eatk
-                if d2hp <= 0:
-                    d2hp = 0
-            elif etarget == 3:
-                print("攻击：[徘徊]-->[无人机03]")
-                d3hp = d3hp - eatk
-                if d3hp <= 0:
-                    d3hp = 0
+                elif etarget == 1:
+                    print("攻击：[徘徊]-->[无人机01]")
+                    d1hp = d1hp - eatk
+                    if d1hp <= 0:
+                        d1hp = 0
+                elif etarget == 2:
+                    print("攻击：[徘徊]-->[无人机02]")
+                    d2hp = d2hp - eatk
+                    if d2hp <= 0:
+                        d2hp = 0
+                elif etarget == 3:
+                    print("攻击：[徘徊]-->[无人机03]")
+                    d3hp = d3hp - eatk
+                    if d3hp <= 0:
+                        d3hp = 0
+
             # round end ally hp result
             print()
             print("[ 回合结束：第",roundnum,"回合 ]")
@@ -378,17 +395,17 @@ if modech == "特殊行动":
 
 
 
-'''
-    if player == "talisman":
-        s1 = "技能1: [刃]"
-        s1d = "     消耗1枚白币，造成6点物理伤害。获得1点充能"
-        s2 = "技能2: [锋]"
-        s2d = "     消耗3枚白币，造成5点物理伤害，每有1架[无人机]额外造成3点物理伤害。获得1点充能"
-        s3 = "技能3: [防御阵型]"
-        s3d = "     消耗3枚黑币，每有1架[无人机]为[夜曲]附属3层[护甲]。获得1点充能"
-        s4 = "技能4: [机身维护]"
-        s4d = "     消耗2枚黑币，为所有[无人机]恢复10点生命值"
-'''
+    '''
+        if player == "talisman":
+            s1 = "技能1: [刃]"
+            s1d = "     消耗1枚白币，造成6点物理伤害。获得1点充能"
+            s2 = "技能2: [锋]"
+            s2d = "     消耗3枚白币，造成5点物理伤害，每有1架[无人机]额外造成3点物理伤害。获得1点充能"
+            s3 = "技能3: [防御阵型]"
+            s3d = "     消耗3枚黑币，每有1架[无人机]为[夜曲]附属3层[护甲]。获得1点充能"
+            s4 = "技能4: [机身维护]"
+            s4d = "     消耗2枚黑币，为所有[无人机]恢复10点生命值"
+    '''
 
 # PVP mode
 if modech == "对决":
